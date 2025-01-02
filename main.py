a931281cfa5f205899d439f8260a6af822c359df
nitrocon <77290402+nitrocon@users.noreply.github.com>
2025-01-02 22:26:03 +0000

    for more exchanges maybe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 from exchanges.coinbase.coinbase import coinbaseAPI
 
-with open('config.json') as config_file:
-    config = json.load(config_file)
+with open('exchanges/coinbase/coinbase.json') as coinbase_config_file:
+    config = json.load(coinbase_config_file)
 base = config['base']
 currency = config['currency']
 coinbaseAPI = coinbaseAPI(base, currency)
